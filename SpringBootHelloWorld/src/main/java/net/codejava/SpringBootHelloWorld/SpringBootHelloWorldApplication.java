429d18023ac847127da22c00d801ffde31dc2b9b
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-02-29 17:39:40 -0500

    primer punto

--- SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -2,12 +2,23 @@ package net.codejava.SpringBootHelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+@RestController
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	@GetMapping
+	public String home() {
+		return "<html><body><p>Home con HTML</p></body></html>";
+	}
+	@RequestMapping("/new")
+	public String newHome() {
+		return "Hola mundo";
+	}
 }
